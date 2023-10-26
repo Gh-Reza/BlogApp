32d1a2eae10d72da37472db8e657fd08eb63344b
Reza Merzaie <ghrezam97@gmail.com>
2023-10-26 11:52:57 +0430

    Create the three_most_recent_posts for the comment model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id'
   has_many :comments
   has_many :likes
+
+  def three_most_recent_posts
+    self.posts.order(created_at: :desc).limit(3)
+  end
 end
