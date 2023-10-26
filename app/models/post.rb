17e21c3522b1a199c0ad9deace8d429f6b976ba4
Reza Merzaie <ghrezam97@gmail.com>
2023-10-26 11:10:41 +0430

    Create the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
-class User < ApplicationRecord
-  has_many: posts, foreign_key: 'author_id'
-  has_many: comments
-  has_many: likes
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :comments
+  has_many :likes
 end
