b39654c4102b56a38203d603b6390293cd01a7ef
Reza Merzaie <ghrezam97@gmail.com>
2023-10-31 20:47:41 +0430

    Add rout, controller, and view for user/:id/posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,4 @@
+class PostsController < ApplicationController
+  def posts
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "users#index"
   get "users/:id", to: "users#user"
+  get "users/:id/posts", to: "posts#posts"
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
