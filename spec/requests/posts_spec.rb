3e53060a75a8919a1dbcb05ecd660f846d853915
Reza Merzaie <ghrezam97@gmail.com>
2023-11-02 00:36:57 +0430

    Add test for posts#show

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -8,5 +8,12 @@ RSpec.describe 'Posts', type: :request do
       expect(response.body).to include('Posts for the specified user')
       expect(response).to render_template(:index)
     end
+
+    it 'return success for /posts/:user_id' do
+      get '/users/23/posts/234'
+      expect(response.status).to eq(200)
+      expect(response.body).to include('The specified post for specified user')
+      expect(response).to render_template(:show)
+    end
   end
 end
