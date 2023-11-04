b129d0cd53cccbb21faebbad4c6b6158fe9eac68
Reza Merzaie <ghrezam97@gmail.com>
2023-11-04 19:04:37 +0430

    Add the form for adding new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "users#index"
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users do
+    resources :posts
   end
 end
