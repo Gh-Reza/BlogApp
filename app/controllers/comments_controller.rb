e2898a7bfd4499b321404536699b08864e277237
Reza Merzaie <ghrezam97@gmail.com>
2023-11-07 18:56:20 +0430

    Removed n+1 problem from comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @current_user = current_user
-    @post = Post.find(params[:post_id])
+    @post = Post.includes(:author, :comments).find(params[:post_id])
     @comment = Comment.new(user_id: @current_user.id, post_id: @post.id, text: params[:comment])
     return unless @comment.save
 
