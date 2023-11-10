8051eceaf5602adedcec0091998972f18aaf9ce7
Reza Merzaie <ghrezam97@gmail.com>
2023-11-10 08:12:54 +0430

    Added confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-
   before_create :set_default_posts_counter
 
   # Include default devise modules. Others available are:
---
+++ spec/mailers/previews/user_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user
+class UserPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user/confirmation
+  def confirmation
+    UserMailer.confirmation
+  end
+
+end
