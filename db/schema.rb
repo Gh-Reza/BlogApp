c262c2e7defd99846bb0c31fc9d170ca09e770ce
Reza Merzaie <ghrezam97@gmail.com>
2023-11-10 04:36:30 +0430

    Added the default value for posts_counter

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_09_221205) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_09_231448) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_09_221205) do
     t.string "name"
     t.string "photo"
     t.text "bio"
-    t.integer "posts_counter"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email", default: "", null: false
